5f7e3a0e6120ea107c7b5d5c873143d8188e1b73
Caleb Stein <caleb@calebstein.net>
2024-03-12 16:58:54 -0700

    store value in reverse for easier math

--- src/giganum.c
+++ src/giganum.c
@@ -7,6 +7,7 @@
 giganum_t* giga_init(char* num_str) {
     int i = 0;
     giganum_t* new_giganum;
+    char* str_reversed;
 
     for (; num_str[i] != '\0'; i++) {
         if (num_str[i] < 0x30 || 0x39 < num_str[i]) {
@@ -24,11 +25,33 @@ giganum_t* giga_init(char* num_str) {
         return NULL;
     }
     new_giganum->ndigits = i;
-    strncpy(new_giganum->val, num_str, i);
+
+    if ((str_reversed = calloc(new_giganum->ndigits, sizeof(char))) == NULL) {
+        perror("calloc");
+        return NULL;
+    }
+
+    for (; i > 0; i--) {
+        str_reversed[new_giganum->ndigits - i] = num_str[i - 1];
+    }
+
+    strncpy(new_giganum->val, str_reversed, new_giganum->ndigits);
+    if (memcmp(new_giganum->val, str_reversed, new_giganum->ndigits) != 0) {
+        free(str_reversed);
+
+        return NULL;
+    }
+    free(str_reversed);
 
     return new_giganum;
 }
 
 void giga_print(giganum_t *giganum) {
-    printf("%s\n", giganum->val);
+    char str_reversed[giganum->ndigits];
+    int i;
+    for (i = giganum->ndigits; i > 0; i--) {
+        str_reversed[giganum->ndigits - i] = giganum->val[i - 1];
+    }
+    printf("%s\n", str_reversed);
+}
 }
