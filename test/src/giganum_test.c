e4d27323be5a3203c83f6b93ab42099b4aec024a
Caleb Stein <caleb@calebstein.net>
2024-03-12 18:25:14 -0700

    add test for giga_add

--- test/src/giganum_test.c
+++ test/src/giganum_test.c
@@ -4,15 +4,23 @@
 #include "giganum.h"
 
 int main() {
-    giganum_t *my_giganum = giga_init("-12345678901234567890123458345349853457349587345435");
-    if (my_giganum == NULL) {
+    giganum_t *my_giganum1 = giga_init("1654787323298409348728723982398742309842398723457265");
+    if (my_giganum1 == NULL) {
+        printf("Something went wrong bruh\n");
+        return -1;
+    }
+    giganum_t *my_giganum2 = giga_init("92346287628347628781298796875437256437428376422234259");
+    if (my_giganum2 == NULL) {
         printf("Something went wrong bruh\n");
         return -1;
     }
 
-    giga_print(my_giganum);
+    giganum_t* add_result = giga_add(my_giganum1, my_giganum2);
+    giga_print(add_result);
 
-    free(my_giganum);
+    free(my_giganum1);
+    free(my_giganum2);
+    free(add_result);
 
     return 0;
 }
